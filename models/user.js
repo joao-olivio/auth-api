d8d4d1caf8330d34029d5577ac12e27bec59d058
João Olivio Neto <joao.olivio@valtech.com>
2018-11-30 16:50:51 -0200

    User model.

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define our model
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type:String,
+        
+    },
+});
+
+// Create the model class
+const exportModel = mongoose.model('user', userSchema);
+
+// Export the model
+module.exports = exportModel;
